03e1ca5671407ef96154b6270483d16924ecc80e
Pureheart Gharoro <gharoropureheart@gmail.com>
2020-05-02 10:58:24 +0100

    fixed merge

--- Client/postit/src/App.js
+++ Client/postit/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Index from './grouppage components/Index';
-=======
 import Main from "./dashBoard components.js/main"
 
->>>>>>> 902b8f49b85c145ffd8e55b240fac45a7b4b6be7
 
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <Index />
-=======
       <div className="App">
-           <Main />
-           
-    </div>
->>>>>>> 902b8f49b85c145ffd8e55b240fac45a7b4b6be7
+        <Main />
+
+      </div>
     </div>
   );
 }
--- Client/postit/src/dashBoard components.js/main.js
+++ Client/postit/src/dashBoard components.js/main.js
@@ -2,13 +2,15 @@ import React from "react"
 import Header from "./Header/header"
 import Body from "./Body/body"
 import Footer from "./Body/bodyComponents/footer"
+import Modal from '../grouppage components/Modal'
 
 
-export default function Main(){
+export default function Main() {
     return (
         <div className="chat-container">
             <Header />
             <Body />
+            <Modal />
             <Footer />
         </div>
     )
