7771c1592dc71d729dfc1585fbc1da6d413593e9
Pureheart Gharoro <gharoropureheart@gmail.com>
2020-05-04 15:16:06 +0100

    Added data files

---
+++ Client/postit/src/data/group.js
@@ -0,0 +1,18 @@
+const groups = [
+    {
+        id: 1,
+        name: 'React',
+        description: 'React class for beginners'
+    },
+    {
+        id: 2,
+        name: 'Node',
+        description: 'NodeJS class for beginners'
+    },
+    {
+        id: 3,
+        name: 'Javascript',
+        description: 'Javascript class for beginners'
+    }
+
+]
---
+++ Client/postit/src/data/messages.js
@@ -0,0 +1,27 @@
+const messages = [
+    {
+        id: 1,
+        sender_id: 3,
+        group_id: 2
+    },
+    {
+        id: 2,
+        sender_id: 2,
+        group_id: 2
+    },
+    {
+        id: 3,
+        sender_id: 1,
+        group_id: 2
+    },
+    {
+        id: 4,
+        sender_id: 1,
+        group_id: 1
+    },
+    {
+        id: 5,
+        sender_id: 3,
+        group_id: 1
+    },
+]
